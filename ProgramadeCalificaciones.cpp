cc1da760132f9ba62d616ff14d9e1d42ae493d08
SharithPadilla <padillasharith28@gmail.com>
2025-06-03 14:03:08 -0600

    Correcion del programa (El codigo anterior no cumplia requisitos)

--- ProgramadeCalificaciones.cpp
+++ ProgramadeCalificaciones.cpp
@@ -1,57 +1,80 @@
-#include <iostream> 
+#include <iostream>
 #include <vector>
-#include <limits> 
-#include <windows.h>
+#include <limits>
+#include <iomanip>
 using namespace std;
 
+struct Alumno {
+    string nombre;
+    string primerApellido;
+    string segundoApellido;
+    int ciclo;
+    int cedula;
+    vector<float> notas;
+    float promedio;
+    string estado;
 
-int main (){
-    string Nombre;
-    string PrimerApellido;
-    string SegundoApellido;
-    string Ciclo;
-    int NCedula;
-    float suma=0;
-    int Alumnos [100],n;
-    double Promedio; 
+};
 
-do {
+bool Validarentrada (string mensaje){
+    int Valor;
 
-SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 11);
-    cout << "¿Cuántos alumnos desea evaluar?" << endl;
-    cin >> n;
-SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
-    if (n <= 0){
-        cout << "Numero no valido. Por favor ingrese un numero mayor que cero." << endl;
-    }   
- } while (n <= 0 );
-  
+    while (true){
+        cout << mensaje;
+        cin >> Valor;
+        if (cin.fail() || Valor <=0){ //cin.fail()  verifica si la entrada falló 
 
-    //for (int i=0; i<n; i++){
-        //cout << "Dijite el nombre completo del estudiante" << endl;
-       // getline (cin, Nombre);
-       // cout << "Dijite los apellidos del estudiante" << endl;
-   // }  
+        cout << "Error: Ingrese un número entero positivo.\n";
 
-for (int i=0; i<n; i++){
-    cout << "¿Ingrese primer nombre?" << endl;
-     cin >> Nombre;
-    cout << "¿Ingrese primer apellido?" << endl;
-     cin >> PrimerApellido;
-    cout << "¿Ingrese segundo apellido?" << endl;
-     cin >> SegundoApellido ;
-    cout << "¿Ingrese ciclo de estudio?" << endl;
-     cin >> Ciclo;
-    cout << "¿Ingrese número de cédula?" << endl;
-     cin >> NCedula;
-}
-
-    //cout << "La cantidad de Alumnos ingresados son: " << Alumnos; 
-
-    
-    if (NCedula <= 0){
-        cout << "Numero no valido. Por favor ingrese un numero mayor que cero." << endl;
+        //limpian el cin
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        } else {
+            return Valor;
+        }
     }
 
-    return 0;
 }
+
+float ValidarNota (string mensaje){
+    float Nota;
+    while (true){
+        cout << mensaje;
+        cin >> Nota;
+        if (cin.fail() || Nota < 0 || Nota > 100) {
+            
+        cout << "Error: La nota debe ser entre 0 y 100.\n";
+
+        cin.clear();
+         cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        } else{
+            return Nota;
+        }
+    }
+}
+
+int main (){
+    int filas;
+    int columnas =5;
+
+    cout << "========================================" << endl;
+    cout << "Bienvenido al sistema de evaluacion de alumnos" << endl;
+    cout << "========================================" << endl;
+    cout << "¿Cuantos alumnos desea evaluar?" << endl;
+    cin >> filas;
+    cout << "Ingrese la nota de los cinco examenes a evaluar" << endl;
+
+    
+        vector<vector<float>> Alumnos(filas, vector<float>(columnas, 0.0));
+
+        //guardar notas
+
+        for (int i=0; i <filas; i++){
+            for (int j=0; j <columnas; j++){
+              float Nota = Alumnos[i][j] = ValidarNota("Ingrese la nota del estudiante #" + to_string(i + 1) + ", examen #" + to_string(j + 1) + ": ");
+
+            }
+        }
+}
+
