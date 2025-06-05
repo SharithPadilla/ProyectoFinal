6daac946351474353e98b3dca1e54835f1937253
SharithPadilla <padillasharith28@gmail.com>
2025-06-04 20:37:11 -0600

    Se agrega el codigo de la estructura de los alumnos, entre otros cambios

--- ProgramadeCalificaciones.cpp
+++ ProgramadeCalificaciones.cpp
@@ -54,27 +54,83 @@ float ValidarNota (string mensaje){
     }
 }
 
+void imprimirReporte(const vector<Alumno>& lista, string estado) {
+    cout << "\n==================== " << estado << " ====================\n";
+    cout << "Nombre completo          Ciclo    Cédula       Promedio\n";
+    cout << "----------------------------------------------------------\n";
+    for (const auto & alumno : lista) {
+        if (alumno.estado == estado) {
+            cout << "> " << alumno.nombre << " " << alumno.primerApellido << " " << alumno.segundoApellido;
+            cout << "   (" << alumno.ciclo << ")     " << alumno.cedula << "     ";
+            cout << fixed << setprecision(2) << alumno.promedio << endl;
+        }
+    }
+}
+
 int main (){
-    int filas;
-    int columnas =5;
+
 
     cout << "========================================" << endl;
     cout << "Bienvenido al sistema de evaluacion de alumnos" << endl;
     cout << "========================================" << endl;
-    cout << "¿Cuantos alumnos desea evaluar?" << endl;
-    cin >> filas;
-    cout << "Ingrese la nota de los cinco examenes a evaluar" << endl;
 
+    float promedio;
+    int filas;
+    int columnas =5;
+    int suma =0;
     
-        vector<vector<float>> Alumnos(filas, vector<float>(columnas, 0.0));
 
+     int N = Validarentrada ("¿Cuántos alumnos desea evaluar?"); 
+     cin >> filas;
+    
+     vector<Alumno> alumnos;
+    
+     for (int i = 0; i < N; ++i) {
+        cout << "\n===== Ingreso de datos del estudiante #" << (i + 1) << " =====\n";
+
+        Alumno a;
+        cout << "Ingrese primer nombre: ";
+        cin >> a.nombre;
+
+        cout << "Ingrese primer apellido: ";
+        cin >> a.primerApellido;
+
+        cout << "Ingrese segundo apellido: ";
+        cin >> a.segundoApellido;
+
+        a.ciclo = Validarentrada("Ingrese ciclo de estudio: ");
+        a.cedula = Validarentrada("Ingrese número de cédula: ");
+
+      
+    }
+        cout << "Ingrese la nota de los cinco examenes a evaluar" << endl;
+
+        vector<vector<float>> notasMatrix(filas, vector<float>(columnas, 0.0));
+        
         //guardar notas
 
         for (int i=0; i <filas; i++){
             for (int j=0; j <columnas; j++){
-              float Nota = Alumnos[i][j] = ValidarNota("Ingrese la nota del estudiante #" + to_string(i + 1) + ", examen #" + to_string(j + 1) + ": ");
+              float Nota = notasMatrix[i][j] = ValidarNota("Ingrese la nota del estudiante #" + to_string(i + 1) + ", examen #" + to_string(j + 1) + ": ");
 
+              for (int i = 0; i < N; i++) {
+
+            vector<Alumno> alumnos;
+            Alumno a;
+            float suma = 0;
+                for (int j = 0; j < 5; ++j) {
+            a.notas.push_back(Nota);
+            suma += Nota;
+          
+        a.promedio = suma / 5.0;
+        a.estado = (a.promedio >= 70.0) ? "APROBADOS" : "REPROBADOS";
+
+   alumnos.push_back(a); 
             }
+    imprimirReporte(alumnos, "APROBADOS");
+    imprimirReporte(alumnos, "REPROBADOS");
         }
+    }
+}
 }
 
